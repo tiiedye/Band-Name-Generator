e549661fb2983ded3be6467b5020f498e5e860a8
tiiedye <tiiedye@gmail.com>
2021-11-18 13:44:40 -0800

    minor fix

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 print("Welcome to the Band Name Generator")
 
 # Ask for the city that they grew up in
-city = input("What city did you grow up in? ")
+city = input("What city did you grow up in?\n")
 
 # Ask the user for a pet's name
-pet_name = input("What is the name of your pet? ")
+pet_name = input("What is the name of your pet?\n")
 
 # Combine the name of the city abd the name of their pet to show them their band name
-print("A possible name for your band could be: " + city + " " + pet_name + "\n")
+print("A possible name for your band could be: " + city + " " + pet_name)

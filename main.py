f9d5742c02ce8479632c79d943bf2f905da6f166
tiiedye <tiiedye@gmail.com>
2021-11-18 13:41:53 -0800

    wrote simple program that saves user input variables and displays them on the terminal

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# Title
+print("Welcome to the Band Name Generator")
 
+# Ask for the city that they grew up in
+city = input("What city did you grow up in? ")
+
+# Ask the user for a pet's name
+pet_name = input("What is the name of your pet? ")
+
+# Combine the name of the city abd the name of their pet to show them their band name
+print("A possible name for your band could be: " + city + " " + pet_name + "\n")
